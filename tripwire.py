062ede51bd330c942b9a9fd47a84012baf04a04d
CheekyClaps <70092074+CheekyClaps@users.noreply.github.com>
2021-04-28 07:18:39 +0000

    Add files via upload

---
+++ tripwire.py
@@ -0,0 +1,50 @@
+#!/bin/env python3
+
+from scapy.all import *
+import sys
+from datetime import datetime
+import socket
+
+
+#import logging
+#import packets
+#import retaliate
+
+# import classes
+#from logging import *
+from packets import *
+from retaliate import *
+from appsettings import *
+
+RED = '\33[31m'
+CYAN = '\33[36m'
+GREEN = '\33[32m'
+WHITE = '\33[0m'
+
+# Init objects
+Settings = AppSettings()
+Packets = Packets()
+
+# Versioning
+__version__ = "0.1"
+
+# Generic timestamp for local logging
+timestamp = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+
+def print_banner():
+    print(f"\n{CYAN}==================================================\n")
+    print(f"{WHITE} TRIPWIRE {CYAN}- a portscan honeypot({WHITE}v{__version__}{CYAN})")
+    print(f"{CYAN} listening on",Settings.iface ,Settings.listening_ip ,Settings.listening_tcp_ports, Settings.listening_udp_ports)
+    print(f"\n=================================================={WHITE} \n")
+
+
+print_banner()
+
+while True:
+ try:
+  sniffer = sniff(lfilter=Packets.build_lfilter, count=0, prn=Packets.parse_packet)
+  # If we Ctrl-C, then exit
+  sys.exit()
+ except socket.error:
+  print('This script must be run as root / Administrator.  Exiting...')
+  sys.exit() 
